0f416f167f7b86fff812d380f8fd9ec6ebe31117
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-01-26 20:19:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def printNos(self, n):
+        # Code here
+        # for i in range(n+1):
+        if n==0:
+            return 
+        print(n, end=' ')
+        self.printNos(n-1)
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printNos(N)
+        print()
+        print("~")
+# } Driver Code Ends
